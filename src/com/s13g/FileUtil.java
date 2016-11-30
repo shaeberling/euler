b9d84b4e1c3f31e0d3afc98a42721e7f553de391
Sascha Haeberling <haeberling@google.com>
2016-11-29 17:57:40 -0800

    Getting started with some of last years Advent of Code questions.

---
+++ src/com/s13g/FileUtil.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016 Sascha Haeberling
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.s13g;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Helper class for reading files that are e.g. required as input for some puzzles.
+ */
+public class FileUtil {
+  public static String readAsString(String filename) throws IOException {
+    FileInputStream inputStream = new FileInputStream(filename);
+    return CharStreams.toString(new InputStreamReader(inputStream, Charsets.UTF_8));
+  }
+}
---
+++ src/com/s13g/aoc2015/Puzzle1_Floors.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2016 Sascha Haeberling
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.s13g.aoc2015;
+
+import com.s13g.FileUtil;
+
+import java.io.IOException;
+
+/**
+ * http://adventofcode.com/2015/day/1
+ */
+public class Puzzle1_Floors {
+  private static final int START_FLOOR = 0;
+
+  public static void main(String[] args) throws IOException {
+    String input = FileUtil.readAsString("data/aoc2015/day1/input_1.txt");
+    char[] inputChars = input.toCharArray();
+
+    int floor = START_FLOOR;
+    boolean part2Solved = false;
+    for (int i = 0; i < inputChars.length; ++i) {
+      if (inputChars[i] == '(') {
+        ++floor;
+      } else if (inputChars[i] == ')') {
+        --floor;
+      }
+      if (!part2Solved && floor == -1) {
+        System.out.println("Part 2 solution: " + (i + 1));
+        part2Solved = true;
+      }
+    }
+    System.out.println("Result: " + floor);
+  }
+}
