637fcf7af6afe53c4a3d8a4467d231c1343d3bd2
Sascha Haeberling <haeberling@google.com>
2016-12-08 01:24:13 -0800

    Start to solve AOC puzzles in Rust. Day 1 both parts done.

---
+++ rust/src/main.rs
@@ -0,0 +1,68 @@
+use std::collections::HashSet;
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn read_file() -> Result<String, io::Error> {
+    let mut f = try!(File::open("../data/aoc/2016/day1.txt"));
+    let mut s = String::new();
+    try!(f.read_to_string(&mut s));
+    Ok(s)
+}
+
+fn process(content: String) {
+    let split: Vec<&str> = content.split(", ").collect();
+    println!("Item num: {}", split.len());
+
+    let mut pos_x:i32 = 0;
+    let mut pos_y:i32= 0;
+    let mut dir_x = 0;
+    let mut dir_y = 1;
+    let mut visited_location = HashSet::new();
+    let mut solution_b :i32 = -1;
+
+    for cmd in &split {
+        let direction = &cmd[..1];
+
+        if dir_x == 0 {
+            dir_x = dir_y * (if direction == "L" { -1 } else { 1 });
+            dir_y = 0;
+        } else {
+            dir_y = dir_x * (if direction == "L" { 1 } else { -1 });
+            dir_x = 0;
+        }
+
+        let amount = &cmd[1..].parse::<i32>().unwrap();
+
+        let mut x :i32 = 0;
+        loop {
+            pos_x += dir_x;
+            pos_y += dir_y;
+            let pos_str = format!("{},{}", pos_x, pos_y);
+
+            if solution_b == -1 && visited_location.contains(&pos_str) {
+                solution_b = pos_x.abs() + pos_y.abs();
+            }
+            visited_location.insert(pos_str);
+
+            x += 1;
+            if &x == amount {
+                break;
+            }
+        }
+    }
+    println!("Solution A: {}, Solution B: {}", pos_x.abs() + pos_y.abs(), solution_b);
+}
+
+fn main() {
+    println!("AOC Puzzle 1");
+
+    let file_result = read_file();
+    match file_result {
+        Ok(file) => process(file),
+        Err(e) => {
+            println!("Ooops. {}", e);
+            return ();
+        },
+    };
+}
