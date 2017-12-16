7a105c9d475ff47bec5ee49d68f49004718270db
Sascha Häberling <mail@sascha-haeberling.com>
2017-12-15 22:34:05 -0800

    Solution for AOC 2017.16

--- go/src/aoc17/aoc17_runner.go
+++ go/src/aoc17/aoc17_runner.go
@@ -1,46 +1,47 @@
 package aoc17
 
 import (
-	//"aoc17/problems/p01"
-	//"aoc17/problems/p02"
-	//"aoc17/problems/p03"
-	//"aoc17/problems/p04"
-	//"aoc17/problems/p05"
-	//"aoc17/problems/p06"
-	//"aoc17/problems/p07"
-	//"aoc17/problems/p08"
-	//"aoc17/problems/p09"
-	//"aoc17/problems/p10"
-	//"aoc17/problems/p11"
-	//"aoc17/problems/p12"
-	//"aoc17/problems/p13"
-	//"aoc17/problems/p14"
-	"runner"
+	"aoc17/problems/p01"
+	"aoc17/problems/p02"
+	"aoc17/problems/p03"
+	"aoc17/problems/p04"
+	"aoc17/problems/p05"
+	"aoc17/problems/p06"
+	"aoc17/problems/p07"
+	"aoc17/problems/p08"
+	"aoc17/problems/p09"
+	"aoc17/problems/p10"
+	"aoc17/problems/p11"
+	"aoc17/problems/p12"
+	"aoc17/problems/p13"
+	"aoc17/problems/p14"
 	"aoc17/problems/p15"
+	"aoc17/problems/p16"
+	"runner"
 )
 
 // Run all the AOC 2017 puzzles
 func Run(dataDir string) {
 	// Note: Add new puzzles here.
 	puzzles := []runner.Puzzle{
-		//{"AOC 2017.01", "aoc/2017/day1.txt", p01.Solve, "995", "1130"},
-		//{"AOC 2017.02", "aoc/2017/day2.txt", p02.Solve, "48357", "351"},
-		//{"AOC 2017.02.hs", "aoc/2017/day2.txt", p02.SolveHs, "48357", "351"},
-		//{"AOC 2017.03", "aoc/2017/day3.txt", p03.Solve, "371", "369601"},
-		//{"AOC 2017.03.big", "aoc/2017/day3.txt", p03.SolveBig, "371", "369601"},
-		//{"AOC 2017.04", "aoc/2017/day4.txt", p04.Solve, "451", "223"},
-		//{"AOC 2017.05", "aoc/2017/day5.txt", p05.Solve, "387096", "28040648"},
-		//{"AOC 2017.06", "aoc/2017/day6.txt", p06.Solve, "6681", "2392"},
-		//{"AOC 2017.07", "aoc/2017/day7.txt", p07.Solve, "dgoocsw", "1275"},
-		//{"AOC 2017.08", "aoc/2017/day8.txt", p08.Solve, "6343", "7184"},
-		//{"AOC 2017.09", "aoc/2017/day9.txt", p09.Solve, "21037", "9495"},
-		//{"AOC 2017.10", "aoc/2017/day10.txt", p10.Solve, "7888", "decdf7d377879877173b7f2fb131cf1b"},
-		//{"AOC 2017.11", "aoc/2017/day11.txt", p11.Solve, "722", "1551"},
-		//{"AOC 2017.12", "aoc/2017/day12.txt", p12.Solve, "175", "213"},
-		//{"AOC 2017.13", "aoc/2017/day13.txt", p13.Solve, "3184", "3878062"},
-		//{"AOC 2017.14", "aoc/2017/day14.txt", p14.Solve, "8140", "1182"},
+		{"AOC 2017.01", "aoc/2017/day1.txt", p01.Solve, "995", "1130"},
+		{"AOC 2017.02", "aoc/2017/day2.txt", p02.Solve, "48357", "351"},
+		{"AOC 2017.02.hs", "aoc/2017/day2.txt", p02.SolveHs, "48357", "351"},
+		{"AOC 2017.03", "aoc/2017/day3.txt", p03.Solve, "371", "369601"},
+		{"AOC 2017.03.big", "aoc/2017/day3.txt", p03.SolveBig, "371", "369601"},
+		{"AOC 2017.04", "aoc/2017/day4.txt", p04.Solve, "451", "223"},
+		{"AOC 2017.05", "aoc/2017/day5.txt", p05.Solve, "387096", "28040648"},
+		{"AOC 2017.06", "aoc/2017/day6.txt", p06.Solve, "6681", "2392"},
+		{"AOC 2017.07", "aoc/2017/day7.txt", p07.Solve, "dgoocsw", "1275"},
+		{"AOC 2017.08", "aoc/2017/day8.txt", p08.Solve, "6343", "7184"},
+		{"AOC 2017.09", "aoc/2017/day9.txt", p09.Solve, "21037", "9495"},
+		{"AOC 2017.10", "aoc/2017/day10.txt", p10.Solve, "7888", "decdf7d377879877173b7f2fb131cf1b"},
+		{"AOC 2017.11", "aoc/2017/day11.txt", p11.Solve, "722", "1551"},
+		{"AOC 2017.12", "aoc/2017/day12.txt", p12.Solve, "175", "213"},
+		{"AOC 2017.13", "aoc/2017/day13.txt", p13.Solve, "3184", "3878062"},
+		{"AOC 2017.14", "aoc/2017/day14.txt", p14.Solve, "8140", "1182"},
 		{"AOC 2017.15", "aoc/2017/day15.txt", p15.Solve, "600", "313"},
-		//{"AOC 2017.16", "aoc/2017/day16.txt", p16.Solve, "", ""},
+		{"AOC 2017.16", "aoc/2017/day16.txt", p16.Solve, "nlciboghjmfdapek", "nlciboghmkedpfja"},
 		//{"AOC 2017.17", "aoc/2017/day17.txt", p17.Solve, "", ""},
 		//{"AOC 2017.18", "aoc/2017/day18.txt", p18.Solve, "", ""},
 		//{"AOC 2017.19", "aoc/2017/day19.txt", p19.Solve, "", ""},
---
+++ go/src/aoc17/problems/p16/p16.go
@@ -0,0 +1,92 @@
+package p16
+
+import (
+	c "common"
+)
+
+func Solve(input string) (string, string) {
+	resultA, resultB := solve(input, 1000000000)
+	return resultA, resultB
+}
+
+func solve(input string, loops int) (string, string) {
+	// Parse input into functions for speed.
+	funcs := parse(input)
+	// Perform one dance.
+	_, resultA := loopIt(1, "", funcs)
+	// See how many iterations until we hit resultA again.
+	numLoops, _ := loopIt(loops, resultA, funcs)
+	// Only perform the remaining operations
+	_, resultB := loopIt(loops%numLoops, "", funcs)
+	return resultA, resultB
+}
+
+func loopIt(num int, match string, funcs []func(arr []rune) []rune) (int, string) {
+	arr := []rune{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'}
+	i := 0
+	for ; i < num; i++ {
+		for _, f := range funcs {
+			arr = f(arr)
+		}
+		if i > 0 && toString(arr) == match {
+			break
+		}
+	}
+	return i, toString(arr)
+}
+
+func toString(arr []rune) (result string) {
+	for _, c := range arr {
+		result += string(c)
+	}
+	return
+}
+
+func partner(arr []rune, a rune, b rune) []rune {
+	for i, v := range arr {
+		if v == a {
+			arr[i] = b
+		} else if v == b {
+			arr[i] = a
+		}
+	}
+	return arr
+}
+
+func exchange(arr []rune, a int, b int) []rune {
+	arr[a], arr[b] = arr[b], arr[a]
+	return arr
+}
+
+func spin(arr []rune, num int) []rune {
+	partA := arr[len(arr)-num:]
+	partB := arr[0: len(arr)-num]
+	return append(partA, partB...)
+}
+
+func parse(input string) []func(arr []rune) []rune {
+	ops := c.SplitByCommaTrim(input)
+	result := make([]func(arr []rune) []rune, len(ops))
+
+	for i, move := range ops {
+		switch move[0] {
+		case 's':
+			x := c.ToIntOrPanic(move[1:])
+			result[i] = func(arr []rune) []rune {
+				return spin(arr, x)
+			}
+		case 'p':
+			parts := c.SplitTrim(move[1:], '/')
+			result[i] = func(arr []rune) []rune {
+				return partner(arr, rune(parts[0][0]), rune(parts[1][0]))
+			}
+		case 'x':
+			parts := c.SplitTrim(move[1:], '/')
+			a, b := c.ToIntOrPanic(parts[0]), c.ToIntOrPanic(parts[1])
+			result[i] = func(arr []rune) []rune {
+				return exchange(arr, a, b)
+			}
+		}
+	}
+	return result
+}
