76a44f75b87136925ee6b31bdcf501226535eacf
Sascha Häberling <mail@sascha-haeberling.com>
2017-12-19 07:43:23 -0800

    Solution for AOC 2017.19 in Go

---
+++ go/src/aoc17/problems/p19/p19.go
@@ -0,0 +1,47 @@
+package p19
+
+import (
+	"strings"
+	"common"
+)
+
+// --- Day 19: A Series of Tubes ---
+// http://adventofcode.com/2017/day/19
+func Solve(input string) (string, string) {
+	resultA, resultB := solveA(strings.Split(input, "\n"))
+	return resultA, common.ToString(resultB)
+}
+
+func solveA(p []string) (string, int) {
+	x, y, dX, dY := strings.Index(p[0], "|"), 0, 0, 1
+	resultA, resultB := "", 0
+
+	isBad := func(newX, newY int) bool {
+		return newX < 0 || newY < 0 || len(p) <= newY || len(p[newY]) <= newX || p[newY][newX] == ' '
+	}
+
+	for ; !isBad(x, y); x, y, resultB = x+dX, y+dY, resultB+1 {
+		if p[y][x] >= 'A' && p[y][x] <= 'Z' {
+			resultA += string(p[y][x])
+		}
+
+		if p[y][x] == '+' {
+			if dX != 0 {
+				dX = 0
+				if !isBad(x, y+1) {
+					dY = 1
+				} else if !isBad(x, y-1) {
+					dY = -1
+				}
+			} else {
+				dY = 0
+				if !isBad(x+1, y) {
+					dX = 1
+				} else if !isBad(x-1, y) {
+					dX = -1
+				}
+			}
+		}
+	}
+	return resultA, resultB
+}
