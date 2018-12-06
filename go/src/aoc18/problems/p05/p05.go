305a5967435ea51d1470ff78c4d8fd6b30f0d35f
Sascha Häberling <mail@sascha-haeberling.com>
2018-12-05 18:06:03 -0800

    Speed up AoC 2018 Day 5 in Go

--- go/src/aoc18/problems/p05/p05.go
+++ go/src/aoc18/problems/p05/p05.go
@@ -3,6 +3,7 @@ package p05
 import (
 	c "common"
 	"strings"
+	"unicode"
 )
 
 // --- Day 5: Alchemical Reduction ---
@@ -11,12 +12,11 @@ func Solve(input string) (string, string) {
 	return c.ToString(solveA(input)), c.ToString(solveB(input))
 }
 
-func solveA(input string) int {
-	lower := strings.ToLower(input)
+func solveA(inputStr string) int {
+	input := []rune(inputStr)
 	for i := 0; i < len(input)-2; i = c.Max(i+1, 0) {
-		if (lower[i] == lower[i+1]) && (input[i] != input[i+1]) {
-			input = input[:i] + input[i+2:]
-			lower = lower[:i] + lower[i+2:]
+		if (unicode.ToLower(input[i]) == unicode.ToLower(input[i+1])) && (input[i] != input[i+1]) {
+			input = append(input[:i], input[i+2:]...)
 			i = i - 2
 		}
 	}
@@ -24,12 +24,11 @@ func solveA(input string) int {
 }
 
 func solveB(input string) int {
-	lower := strings.ToLower(input)
 	abc := "abcdefghijklmnopqrstuvwxyz"
 	ch := make(chan int)
 	// Parallelize solving the problem for each letter.
 	for _, r := range abc {
-		go solveBForLetter(&input, &lower, r, ch)
+		go solveBForLetter(&input, r, ch)
 	}
 
 	// Wait for results from goroutines and find the smallest.
@@ -40,12 +39,7 @@ func solveB(input string) int {
 	return smallest
 }
 
-func solveBForLetter(input *string, lower *string, r rune, ch chan<- int) {
-	newInput := ""
-	for i, r2 := range *lower {
-		if r != r2 {
-			newInput += string((*input)[i])
-		}
-	}
-	ch <- solveA(newInput)
+func solveBForLetter(input *string, r rune, ch chan<- int) {
+	var replacer = strings.NewReplacer(string(r), "", string(unicode.ToUpper(r)), "")
+	ch <- solveA(replacer.Replace(*input))
 }
