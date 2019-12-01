84eb63d6e7f8fab81c2488bc5bcaba7aa0686ffd
Sascha Häberling <mail@sascha-haeberling.com>
2019-11-30 19:44:01 -0800

    AOC 2018 VM implementation

--- kotlin/src/com/s13g/aoc/aoc2018/AocVm.kt
+++ kotlin/src/com/s13g/aoc/aoc2018/AocVm.kt
@@ -1,4 +1,140 @@
 package com.s13g.aoc.aoc2018
 
-class AocVm {
+
+class AocVm(private val ipIdx: Int, private val program: List<Instruction>) {
+  private var ip = 0
+  private val reg = intArrayOf(0, 0, 0, 0, 0, 0)
+  private val ops = createOperations()
+
+  fun runUntilHalt() {
+    while (true) {
+      if (!next()) break
+    }
+  }
+
+  /** Returns false, if the program execution halted */
+  private fun next(): Boolean {
+    if (program.size <= ip) return false
+    val instr = program[ip]
+//    val preDbgStr = "ip=$ip [${reg.joinToString(", ")}] $instr"
+    reg[ipIdx] = ip
+    ops[instr.op]!!(instr.params, reg)
+    ip = reg[ipIdx]
+    ip++
+//    println("$preDbgStr [${reg.joinToString(", ")}]")
+    return true
+  }
+
+  fun getReg(i: Int) = reg[i]
+  fun setReg(i: Int, v: Int) {
+    reg[i] = v
+  }
+}
+
+fun parseInstructions(program: List<String>): List<Instruction> {
+  val result = arrayListOf<Instruction>()
+  for (line in program) {
+    if (line.startsWith("#")) continue
+    val split = line.split(" ")
+    assert(split.size == 4) { println("Invalid instruction: '$line'") }
+    result.add(Instruction(split[0],
+        Params(split[1].toInt(), split[2].toInt(), split[3].toInt())))
+  }
+  return result
+}
+
+data class Instruction(val op: String, val params: Params) {
+  override fun toString() = "$op $params"
+}
+
+data class Params(val a: Int, val b: Int, val c: Int) {
+  override fun toString() = "$a $b $c"
+}
+
+typealias Operation = (Params, IntArray) -> Unit
+
+private fun createOperations(): HashMap<String, Operation> {
+  val operations =
+      hashMapOf<String, Operation>()
+  operations["addr"] = ::addr
+  operations["addi"] = ::addi
+  operations["mulr"] = ::mulr
+  operations["muli"] = ::muli
+  operations["banr"] = ::banr
+  operations["bani"] = ::bani
+  operations["borr"] = ::borr
+  operations["bori"] = ::bori
+  operations["setr"] = ::setr
+  operations["seti"] = ::seti
+  operations["gtir"] = ::gtir
+  operations["gtri"] = ::gtri
+  operations["gtrr"] = ::gtrr
+  operations["eqir"] = ::eqir
+  operations["eqri"] = ::eqri
+  operations["eqrr"] = ::eqrr
+  return operations
+}
+
+private fun addr(param: Params, reg: IntArray) {
+  reg[param.c] = reg[param.a] + reg[param.b]
+}
+
+private fun addi(param: Params, reg: IntArray) {
+  reg[param.c] = reg[param.a] + param.b
+}
+
+private fun mulr(param: Params, reg: IntArray) {
+  reg[param.c] = reg[param.a] * reg[param.b]
+}
+
+private fun muli(param: Params, reg: IntArray) {
+  reg[param.c] = reg[param.a] * param.b
+}
+
+private fun banr(param: Params, reg: IntArray) {
+  reg[param.c] = reg[param.a] and reg[param.b]
+}
+
+private fun bani(param: Params, reg: IntArray) {
+  reg[param.c] = reg[param.a] and param.b
+}
+
+private fun borr(param: Params, reg: IntArray) {
+  reg[param.c] = reg[param.a] or reg[param.b]
+}
+
+private fun bori(param: Params, reg: IntArray) {
+  reg[param.c] = reg[param.a] or param.b
+}
+
+private fun setr(param: Params, reg: IntArray) {
+  reg[param.c] = reg[param.a]
+}
+
+private fun seti(param: Params, reg: IntArray) {
+  reg[param.c] = param.a
+}
+
+private fun gtir(param: Params, reg: IntArray) {
+  reg[param.c] = if (param.a > reg[param.b]) 1 else 0
+}
+
+private fun gtri(param: Params, reg: IntArray) {
+  reg[param.c] = if (reg[param.a] > param.b) 1 else 0
+}
+
+private fun gtrr(param: Params, reg: IntArray) {
+  reg[param.c] = if (reg[param.a] > reg[param.b]) 1 else 0
+}
+
+private fun eqir(param: Params, reg: IntArray) {
+  reg[param.c] = if (param.a == reg[param.b]) 1 else 0
+}
+
+private fun eqri(param: Params, reg: IntArray) {
+  reg[param.c] = if (reg[param.a] == param.b) 1 else 0
+}
+
+private fun eqrr(param: Params, reg: IntArray) {
+  reg[param.c] = if (reg[param.a] == reg[param.b]) 1 else 0
 }
