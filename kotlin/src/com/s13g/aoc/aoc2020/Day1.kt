46e2599a25594e1ea674bc838abebfe90964c6e8
Sascha Häberling <mail@sascha-haeberling.com>
2020-11-30 22:34:31 -0800

    AoC 2020.01 in Kotlin

--- kotlin/src/com/s13g/aoc/aoc2020/Day1.kt
+++ kotlin/src/com/s13g/aoc/aoc2020/Day1.kt
@@ -2,9 +2,30 @@ package com.s13g.aoc.aoc2020
 
 import com.s13g.aoc.Result
 import com.s13g.aoc.Solver
+import com.s13g.aoc.mul
 
+/**
+ * --- Day 1: Report Repair ---
+ * https://adventofcode.com/2020/day/1
+ */
 class Day1 : Solver {
-    override fun solve(lines: List<String>): Result {
-        TODO("Not yet implemented")
+  override fun solve(lines: List<String>): Result {
+    val (resultA, resultB) = run(lines.map { it.toLong() })
+    return Result("$resultA", "$resultB")
+  }
+
+  private fun run(input: List<Long>): Array<Long> {
+    val result = arrayOf(0L, 0L)
+    for (i in input.indices) {
+      for (j in i + 1 until input.size) {
+        (result[0] == 0L).let { if (it) result[0] = test(listOf(input[i], input[j])) }
+        for (k in j + 1 until input.size) {
+          (result[1] == 0L).let { if (it) result[1] = test(listOf(input[i], input[j], input[k])) }
+        }
+      }
     }
+    return result
+  }
+
+  private fun test(items: List<Long>) = if (items.sum() == 2020L) items.mul() else 0
 }
--- kotlin/src/com/s13g/aoc/aoc2020/RunAllAoc2020.kt
+++ kotlin/src/com/s13g/aoc/aoc2020/RunAllAoc2020.kt
@@ -2,10 +2,14 @@ package com.s13g.aoc.aoc2020
 
 import com.s13g.aoc.PuzzleRunner
 
-private const val ONLY_RUN_NEW = true
+private const val ONLY_RUN_NEW = false
 
+/**
+ * --- Advent of Code 2020 ---
+ * https://adventofcode.com/2020
+ */
 fun main() {
   val runner = PuzzleRunner(ONLY_RUN_NEW, 2020)
-  runner.addProblem(1, Day1(), "", "")
+  runner.addProblem(1, Day1(), "787776", "262738554")
   runner.run()
 }
