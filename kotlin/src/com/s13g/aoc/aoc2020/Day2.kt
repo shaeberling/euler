1342cc4eab351d3984871a68514434b04486d993
Sascha Häberling <mail@sascha-haeberling.com>
2020-12-01 21:50:31 -0800

    AoC 2020.02 in Kotlin

---
+++ kotlin/src/com/s13g/aoc/aoc2020/Day2.kt
@@ -0,0 +1,29 @@
+package com.s13g.aoc.aoc2020
+
+import com.s13g.aoc.Result
+import com.s13g.aoc.Solver
+
+/**
+ * --- Day 2: Password Philosophy ---
+ * https://adventofcode.com/2020/day/2
+ */
+class Day2 : Solver {
+  private val regex = """(\d+)-(\d+) (.): (.*)""".toRegex()
+
+  override fun solve(lines: List<String>): Result {
+    val input = lines.map { parse(it) }
+    val resultA = input.map { it.isLegalA() }.count { it }
+    val resultB = input.map { it.isLegalB() }.count { it }
+    return Result("$resultA", "$resultB")
+  }
+
+  private fun parse(row: String): Rule {
+    val (a, b, ch, pass) = regex.find(row)!!.destructured
+    return Rule(a.toInt(), b.toInt(), ch[0], pass)
+  }
+}
+
+private data class Rule(val a: Int, val b: Int, val ch: Char, val pass: String)
+
+private fun Rule.isLegalA() = pass.filter { it == ch }.count().let { it in a..b }
+private fun Rule.isLegalB() = (pass.length >= a && pass[a - 1] == ch) xor (pass.length >= b && pass[b - 1] == ch)
--- kotlin/src/com/s13g/aoc/aoc2020/RunAllAoc2020.kt
+++ kotlin/src/com/s13g/aoc/aoc2020/RunAllAoc2020.kt
@@ -2,7 +2,7 @@ package com.s13g.aoc.aoc2020
 
 import com.s13g.aoc.PuzzleRunner
 
-private const val ONLY_RUN_NEW = false
+private const val ONLY_RUN_NEW = true
 
 /**
  * --- Advent of Code 2020 ---
@@ -11,5 +11,6 @@ private const val ONLY_RUN_NEW = false
 fun main() {
   val runner = PuzzleRunner(ONLY_RUN_NEW, 2020)
   runner.addProblem(1, Day1(), "787776", "262738554")
+  runner.addProblem(2, Day2(), "396", "428")
   runner.run()
 }
