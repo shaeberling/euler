e6346b0a62012f410d4e3fc5349f488ba9a11ec9
Sascha Häberling <mail@sascha-haeberling.com>
2020-12-02 21:28:15 -0800

    AoC 2020.03 in Kotlin

--- kotlin/src/com/s13g/aoc/aoc2020/RunAllAoc2020.kt
+++ kotlin/src/com/s13g/aoc/aoc2020/RunAllAoc2020.kt
@@ -2,7 +2,7 @@ package com.s13g.aoc.aoc2020
 
 import com.s13g.aoc.PuzzleRunner
 
-private const val ONLY_RUN_NEW = true
+private const val ONLY_RUN_NEW = false
 
 /**
  * --- Advent of Code 2020 ---
@@ -12,5 +12,6 @@ fun main() {
   val runner = PuzzleRunner(ONLY_RUN_NEW, 2020)
   runner.addProblem(1, Day1(), "787776", "262738554")
   runner.addProblem(2, Day2(), "396", "428")
+  runner.addProblem(3, Day3(), "282", "958815792")
   runner.run()
 }
