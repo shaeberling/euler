1935e20ccf25796ba28dd8248f17f6c02e7e1f0a
Sascha Häberling <mail@sascha-haeberling.com>
2020-12-05 02:57:17 -0800

    Technical bugfix and added comment.

--- kotlin/src/com/s13g/aoc/aoc2020/Day5.kt
+++ kotlin/src/com/s13g/aoc/aoc2020/Day5.kt
@@ -4,19 +4,21 @@ import com.s13g.aoc.Result
 import com.s13g.aoc.Solver
 
 /**
- * --- Day 5:  ---
+ * --- Day 5: Binary Boarding ---
  * https://adventofcode.com/2020/day/5
  */
 class Day5 : Solver {
 
   override fun solve(lines: List<String>): Result {
+    // Note, if I would have been clever I would have realized that this is just a binary encoding.
+    // In any case, I am leaving this solution up as I want to show what I did to solve the problem int he moment.
     val seats = lines.map { calcSeat(it) }.map { it.first * 8 + it.second }.toSet()
     return Result("${seats.max()}", "${findSeat(seats)}")
   }
 
   // Solution for Part B
   private fun findSeat(occupiedSeats: Set<Int>): Int {
-    for (x in 1..(127 * 7)) if (occupiedSeats.contains(x - 1) && !occupiedSeats.contains(x)) return x
+    for (x in 1..(127 * 8)) if (occupiedSeats.contains(x - 1) && !occupiedSeats.contains(x)) return x
     error("Cannot find free seat.")
   }
 }
