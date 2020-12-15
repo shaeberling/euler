868359e7b3eed2c2c6332c6dec2b5a5da744b421
Sascha Häberling <mail@sascha-haeberling.com>
2020-12-14 20:57:47 -0800

    Make all methods and helper classes class-private.

--- kotlin/src/com/s13g/aoc/aoc2020/Day8.kt
+++ kotlin/src/com/s13g/aoc/aoc2020/Day8.kt
@@ -12,56 +12,57 @@ class Day8 : Solver {
     val input = lines.map { it.split(" ") }.map { Instr(it[0], it[1].toInt()) }
     return Result("${runA(VM(input))}", "${runB(input)}")
   }
-}
 
-private fun runA(vm: VM): Int {
-  while (vm.step()) if (vm.hasLooped()) return vm.acc
-  error("No loop detected")
-}
 
-private fun runB(program: List<Instr>): Int {
-  for (x in program.indices) {
-    // Clone the list.
-    val variant = program.toMutableList()
-
-    // Swap the instruction if it matches.
-    if (variant[x].cmd == "jmp") variant[x] = Instr("nop", variant[x].value)
-    else if (variant[x].cmd == "nop") variant[x] = Instr("jmp", variant[x].value)
-
-    // Create a Vm an run it until it either ends or loops.
-    val vm = VM(variant)
-    while (vm.step() && !vm.hasLooped());
-
-    // If the program has ended normally, we found the fix!
-    if (vm.hasEnded()) return vm.acc
+  private fun runA(vm: VM): Int {
+    while (vm.step()) if (vm.hasLooped()) return vm.acc
+    error("No loop detected")
   }
-  error("Could not find a solution for Part B")
-}
 
-private class VM(val program: List<Instr>) {
-  var acc = 0
-  var pc = 0
+  private fun runB(program: List<Instr>): Int {
+    for (x in program.indices) {
+      // Clone the list.
+      val variant = program.toMutableList()
 
-  private val history = mutableSetOf<Int>()
+      // Swap the instruction if it matches.
+      if (variant[x].cmd == "jmp") variant[x] = Instr("nop", variant[x].value)
+      else if (variant[x].cmd == "nop") variant[x] = Instr("jmp", variant[x].value)
 
-  /** Returns whether the program is still running. False if it has finished. */
-  fun step(): Boolean {
-    history.add(pc)
-    when (program[pc].cmd) {
-      "nop" -> pc++
-      "acc" -> {
-        acc += program[pc].value; pc++
-      }
-      "jmp" -> pc += program[pc].value
+      // Create a Vm an run it until it either ends or loops.
+      val vm = VM(variant)
+      while (vm.step() && !vm.hasLooped());
+
+      // If the program has ended normally, we found the fix!
+      if (vm.hasEnded()) return vm.acc
     }
-    return !hasEnded()
+    error("Could not find a solution for Part B")
   }
 
-  /** Whether the instruction to be executed has already been executed before. */
-  fun hasLooped() = history.contains(pc)
+  private class VM(val program: List<Instr>) {
+    var acc = 0
+    var pc = 0
 
-  /** Whether this program has terminated normally .*/
-  fun hasEnded() = pc >= program.size
+    private val history = mutableSetOf<Int>()
+
+    /** Returns whether the program is still running. False if it has finished. */
+    fun step(): Boolean {
+      history.add(pc)
+      when (program[pc].cmd) {
+        "nop" -> pc++
+        "acc" -> {
+          acc += program[pc].value; pc++
+        }
+        "jmp" -> pc += program[pc].value
+      }
+      return !hasEnded()
+    }
+
+    /** Whether the instruction to be executed has already been executed before. */
+    fun hasLooped() = history.contains(pc)
+
+    /** Whether this program has terminated normally .*/
+    fun hasEnded() = pc >= program.size
+  }
+
+  private data class Instr(val cmd: String, val value: Int)
 }
-
-private data class Instr(val cmd: String, val value: Int)
