e9289cf5f94bae366c0fe632ecd17c7670e43121
Sascha Häberling <mail@sascha-haeberling.com>
2020-12-19 01:09:55 -0800

    AoC 2015.04 in Kotlin

--- kotlin/src/com/s13g/aoc/aoc2015/RunAllAoc2015.kt
+++ kotlin/src/com/s13g/aoc/aoc2015/RunAllAoc2015.kt
@@ -2,7 +2,7 @@ package com.s13g.aoc.aoc2015
 
 import com.s13g.aoc.PuzzleRunner
 
-private const val ONLY_RUN_NEW = false
+private const val ONLY_RUN_NEW = true
 
 /**
  * ---- Advent of Code 2015 ----
@@ -11,5 +11,6 @@ private const val ONLY_RUN_NEW = false
 fun main() {
   val runner = PuzzleRunner(ONLY_RUN_NEW, 2015)
   runner.addProblem(1, Day1(), "138", "1771")
+  runner.addProblem(4, Day4(), "117946", "3938038")
   runner.run()
 }
