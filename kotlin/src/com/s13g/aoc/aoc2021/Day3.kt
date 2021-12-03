19c1c646257d5679d48349507a43a16639eda11c
Sascha Häberling <mail@sascha-haeberling.com>
2021-12-02 22:43:51 -0800

    Using more functional paradigms.

--- kotlin/src/com/s13g/aoc/aoc2021/Day3.kt
+++ kotlin/src/com/s13g/aoc/aoc2021/Day3.kt
@@ -10,14 +10,9 @@ import com.s13g.aoc.resultFrom
  */
 class Day3 : Solver {
   override fun solve(lines: List<String>): Result {
-    var gamma = ""
-    var epsilon = ""
-    for (i in lines[0].indices) {
-      val cnt = lines.fold(Counter(0, 0)) { cnt, s -> cnt.add(s[i]) }
-      gamma += if (cnt.ones > cnt.zeros) "1" else "0"
-      epsilon += if (cnt.ones > cnt.zeros) "0" else "1"
-    }
-    val partA = gamma.toInt(2) * epsilon.toInt(2)
+    val ge = lines[0].indices.map { i -> lines.fold(Counter(0, 0)) { cnt, s -> cnt.add(s[i]) } }
+                             .fold(GammaEpsi("", "")) { res, cnt -> res.add(cnt) }
+    val partA = ge.g.toInt(2) * ge.e.toInt(2)
     val partB = processB(lines, 0 /* Most common */) * processB(lines, 1 /* Least common */)
     return resultFrom(partA, partB)
   }
@@ -33,8 +28,12 @@ class Day3 : Solver {
     return ratingValues[0].toInt(2)
   }
 
+  private data class GammaEpsi(val g: String, val e: String) {
+    fun add(cnt: Counter) = cnt.hiLow().let { GammaEpsi(this.g + it[0], this.e + it[1]) }
+  }
+
   private data class Counter(val zeros: Int, val ones: Int) {
     fun add(c: Char) = if (c == '0') Counter(this.zeros + 1, this.ones) else Counter(this.zeros, this.ones + 1)
-    fun hiLow() = listOf(if (this.zeros > this.ones) '0' else '1', if (this.zeros > this.ones) '1' else '0')
+    fun hiLow() = if (this.zeros > this.ones) listOf('0', '1') else listOf('1', '0')
   }
 }
