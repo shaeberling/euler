9594b4f7bafa73d96c434137cde66011252320b6
Sascha Häberling <mail@sascha-haeberling.com>
2021-12-06 22:07:24 -0800

    AoC 2021.07 in Kotlin

--- kotlin/src/com/s13g/aoc/aoc2021/RunAllAoc2021.kt
+++ kotlin/src/com/s13g/aoc/aoc2021/RunAllAoc2021.kt
@@ -16,7 +16,7 @@ fun main() {
   runner.addProblem(4, Day4(), "33462", "30070")
   runner.addProblem(5, Day5(), "5169", "22083")
   runner.addProblem(6, Day6(), "383160", "1721148811504")
-//  runner.addProblem(7, Day7(), "", "", true)
+  runner.addProblem(7, Day7(), "329389", "86397080")
 //  runner.addProblem(8, Day8(), "", "", true)
 //  runner.addProblem(9, Day9(), "", "", true)
 //  runner.addProblem(10, Day10(), "", "", true)
