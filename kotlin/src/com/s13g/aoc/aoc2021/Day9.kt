3a3ba3cfbde8d83f925de3bbc47918dfdbb22926
Sascha Häberling <mail@sascha-haeberling.com>
2021-12-09 20:42:54 -0800

    Add visualization for Day 9. Will produce a PNG with result.

--- kotlin/src/com/s13g/aoc/aoc2021/Day9.kt
+++ kotlin/src/com/s13g/aoc/aoc2021/Day9.kt
@@ -3,6 +3,11 @@ package com.s13g.aoc.aoc2021
 import com.s13g.aoc.Result
 import com.s13g.aoc.Solver
 import com.s13g.aoc.mul
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+
 
 /**
  * --- Day 9: Smoke Basin ---
@@ -43,6 +48,7 @@ class Day9 : Solver {
     val flownIntoResult = flownInto.keys.associateWith { allThatFlowInto(it, flownInto) }
     // Take top 3 sizes and multiply them together.
     val partB = flownIntoResult.map { it.value.size }.sortedDescending().take(3).mul()
+    writeImage(input, flownIntoResult)
     return Result("$partA", "$partB")
   }
 
@@ -54,6 +60,21 @@ class Day9 : Solver {
       result.addAll(allThatFlowInto(flowsInto, flownInto))
     }
     return result
+  }
+
+  // Just for fun...
+  private fun writeImage(puzzle: PuzzleInput, basins: Map<XY, Set<XY>>) {
+    val winningPositions = basins.map { it.value }.sortedByDescending { it.size }.take(3).flatten().toSet()
+    val img = BufferedImage(puzzle.width, puzzle.height, BufferedImage.TYPE_INT_RGB)
+    for (x in 0 until puzzle.width) {
+      for (y in 0 until puzzle.height) {
+        val hue = if (winningPositions.contains(XY(x, y))) 1f else 0.5f
+        img.setRGB(x, y, Color.HSBtoRGB(hue, 1f, 1f - puzzle.height(XY(x, y)) / 9f))
+      }
+    }
+    val file = File("Day9.png")
+    println(file.absolutePath)
+    ImageIO.write(img, "png", file)
   }
 }
 
