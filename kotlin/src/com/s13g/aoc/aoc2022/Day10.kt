43689e7489bc1b2b17b207513491403fa09382ea
Sascha Häberling <mail@sascha-haeberling.com>
2022-12-10 00:38:22 -0800

    AoC 2022.11 in Kotlin

---
+++ kotlin/src/com/s13g/aoc/aoc2022/Day10.kt
@@ -0,0 +1,35 @@
+package com.s13g.aoc.aoc2022
+
+import com.s13g.aoc.Result
+import com.s13g.aoc.Solver
+import kotlin.math.abs
+import kotlin.math.floor
+
+class Day10 : Solver {
+  private var regX = 1
+  private var cycle = 0
+  private val cycleVals = mutableListOf<Int>()
+  private val crt = mutableListOf<StringBuilder>()
+
+  private fun cycle() {
+    val row = (floor(cycle / 40f)).toInt()
+    val col = cycle % 40
+    if (abs(col - regX.coerceIn(0, 40)) <= 1) crt[row].setCharAt(col, '#')
+    cycle++
+    if (cycle <= 220 && cycle % 40 == 20) cycleVals.add(cycle * regX)
+  }
+
+  override fun solve(lines: List<String>): Result {
+    (1..6).forEach { _ -> crt.add(StringBuilder(".".repeat(40))) }
+    for (line in lines) {
+      val split = line.split(" ")
+      val instr = split[0]
+      if (instr == "addx") {
+        for (i in 1..2) cycle()
+        regX += split[1].toInt()
+      } else cycle()
+    }
+    val part2 = "\n" + crt.joinToString("\n") { it.toString() }
+    return Result(cycleVals.sum().toString(), part2)
+  }
+}
--- kotlin/src/com/s13g/aoc/aoc2022/RunAllAoc2022.kt
+++ kotlin/src/com/s13g/aoc/aoc2022/RunAllAoc2022.kt
@@ -4,6 +4,15 @@ import com.s13g.aoc.PuzzleRunner
 
 private const val ONLY_RUN_NEW = true
 
+val DAY_10_PART_2 = """
+####.#..#.###..####.#....###....##.###..
+#....#..#.#..#....#.#....#..#....#.#..#.
+###..####.###....#..#....#..#....#.#..#.
+#....#..#.#..#..#...#....###.....#.###..
+##...#..#.#..#.#....#....#.#..#..#.#.#..
+####.#..#.###..####.####.#..#..##..#..#."""
+
+
 /**
  * ---- Advent of Code 2022 ----
  * https://adventofcode.com/2022
@@ -11,15 +20,15 @@ private const val ONLY_RUN_NEW = true
 fun main() {
   val runner = PuzzleRunner(ONLY_RUN_NEW, 2022)
   runner.addProblem(1, Day1(), "71124", "204639")
-   runner.addProblem(2, Day2(), "9177", "12111")
-   runner.addProblem(3, Day3(), "7903", "2548")
-   runner.addProblem(4, Day4(), "475", "825")
-   runner.addProblem(5, Day5(), "SHQWSRBDL", "CDTQZHBRS")
-   runner.addProblem(6, Day6(), "1892", "2313")
-   runner.addProblem(7, Day7(), "1743217", "8319096")
-   runner.addProblem(8, Day8(), "1814", "330786")
-   runner.addProblem(9, Day9(), "6367", "2536", true)
-//   runner.addProblem(10, Day10(), "", "", true)
+  runner.addProblem(2, Day2(), "9177", "12111")
+  runner.addProblem(3, Day3(), "7903", "2548")
+  runner.addProblem(4, Day4(), "475", "825")
+  runner.addProblem(5, Day5(), "SHQWSRBDL", "CDTQZHBRS")
+  runner.addProblem(6, Day6(), "1892", "2313")
+  runner.addProblem(7, Day7(), "1743217", "8319096")
+  runner.addProblem(8, Day8(), "1814", "330786")
+  runner.addProblem(9, Day9(), "6367", "2536")
+  runner.addProblem(10, Day10(), "12640", DAY_10_PART_2, true)
   // runner.addProblem(11, Day11(), "", "", true)
   // runner.addProblem(12, Day12(), "", "", true)
   // runner.addProblem(13, Day13(), "", "", true)
@@ -36,5 +45,4 @@ fun main() {
   // runner.addProblem(24, Day24(), "", "", true)
   // runner.addProblem(25, Day25(), "", "", true)
   runner.run()
-
 }
