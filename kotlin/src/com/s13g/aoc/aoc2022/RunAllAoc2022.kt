df6d1bdbfafa647a841a71323a18a11d34150136
Sascha Häberling <mail@sascha-haeberling.com>
2022-12-24 02:50:05 -0800

    AoC 2022.23 in Kotlin

--- kotlin/src/com/s13g/aoc/aoc2022/RunAllAoc2022.kt
+++ kotlin/src/com/s13g/aoc/aoc2022/RunAllAoc2022.kt
@@ -39,10 +39,10 @@ fun main() {
   runner.addProblem(18, Day18(), "4604", "2604")
   runner.addProblem(19, Day19(), "1427", "4400")
   runner.addProblem(20, Day20(), "10831", "6420481789383")
-   runner.addProblem(21, Day21(), "84244467642604", "3759569926192")
+  runner.addProblem(21, Day21(), "84244467642604", "3759569926192")
   // runner.addProblem(22, Day22(), "", "", true)
-//   runner.addProblem(23, Day23(), "", "", true)
-   runner.addProblem(24, Day24(), "260", "747", true)
+  runner.addProblem(23, Day23(), "4236", "1023" , true)
+  runner.addProblem(24, Day24(), "260", "747")
   // runner.addProblem(25, Day25(), "", "", true)
   runner.run()
 }
