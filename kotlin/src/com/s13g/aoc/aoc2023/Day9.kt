cf071eb6dbc5d364d9a1d5cd4f50c736a5369bb3
Sascha Häberling <mail@sascha-haeberling.com>
2023-12-09 19:16:36 +0100

    Shorter AoC 2023.09 in Kotlin

--- kotlin/src/com/s13g/aoc/aoc2023/Day9.kt
+++ kotlin/src/com/s13g/aoc/aoc2023/Day9.kt
@@ -11,27 +11,19 @@ import com.s13g.aoc.resultFrom
 class Day9 : Solver {
   override fun solve(lines: List<String>): Result {
     val histories = lines.map { it.split(" ").map { x -> x.toLong() } }
-    val partA = histories.sumOf { getNextNum(it).first }
-    val partB = histories.sumOf { getNextNum(it).second }
+    val partA = histories.sumOf { getNextNum(it) }
+    val partB = histories.map { it.reversed() }.sumOf { getNextNum(it) }
     return resultFrom(partA, partB)
   }
 
-  private fun getNextNum(hist: List<Long>): Pair<Long, Long> {
+  private fun getNextNum(hist: List<Long>): Long {
     val data = mutableListOf(hist)
-
     // Create all the new lines until all are 0.
     while (data.last().count { it != 0L } != 0) {
       val derivative = data.last().windowed(2, 1).map { it[1] - it[0] }.toList()
       data.add(derivative)
     }
-
     // Build back up by attaching the last digit upwards.
-    val lastNum = data.reversed().sumOf { it.last() }
-    // For part b, do the same thing for the beginning.
-    var firstNum = 0L
-    for (list in data.reversed()) {
-      firstNum = list.first() - firstNum
-    }
-    return Pair(lastNum, firstNum)
+    return data.reversed().sumOf { it.last() }
   }
 }
